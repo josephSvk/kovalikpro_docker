a6241cfc9d84c3ee16b2bc11cd107ee1ef75f663
josephsvk <20kovalik20@gmail.com>
2024-03-22 17:28:10 +0100

    master

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="6WEXJAwtidrpas2dW7dptoaZ3V5NkG4eJbKbOE3DMlT8041p3BBFy5TaNESYHCQR",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "kovalik.pro.local"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
